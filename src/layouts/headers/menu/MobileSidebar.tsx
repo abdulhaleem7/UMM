114192c3e44eca60e68486d03042836b74e6f072
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2025-05-09 22:44:06 +0100

    Update social media links in MobileSidebar component for accuracy and consistency

--- src/layouts/headers/menu/MobileSidebar.tsx
+++ src/layouts/headers/menu/MobileSidebar.tsx
@@ -17,7 +17,7 @@ const MobileSidebar = ({ isActive, setIsActive }: MobileSidebarProps) => {
         <nav className="tgmobile__menu-box">
           <div onClick={() => setIsActive(false)} className="close-btn"><i className="tg-flaticon-close"></i></div>
           <div className="nav-logo">
-            <Link href="/"><Image src={logo} alt="Logo" style={{"width": "100px"}}/></Link>
+            <Link href="/"><Image src={logo} alt="Logo" style={{ "width": "100px" }} /></Link>
           </div>
           <div className="tgmobile__search">
             <form onSubmit={(e) => e.preventDefault()}>
@@ -27,14 +27,20 @@ const MobileSidebar = ({ isActive, setIsActive }: MobileSidebarProps) => {
           </div>
           <div className="tgmobile__menu-outer">
             <NavMenu />
+            <li>
+              <div className="tgmenu__navbar-wrap tgmenu__main-menu m-10" style={{ "marginLeft": "20px" }}>
+                <Link href="/request-a-quote" className="btn" style={{ "color": "white" }}>Request a quote</Link>
+              </div>
+            </li>
           </div>
           <div className="social-links">
             <ul className="list-wrap">
-              <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-              <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-              <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-              <li><a href="#"><i className="fab fa-linkedin-in"></i></a></li>
-              <li><a href="#"><i className="fab fa-youtube"></i></a></li>
+              <li><a href="https://www.facebook.com/share/18xBbghrF6/?mibextid=wwXIfr"><i className="fab fa-facebook-f"></i></a></li>
+              <li><a href="https://x.com/unifiedmovers?s=21"><i className="fab fa-x-twitter"></i></a></li>
+              <li><a href="https://www.instagram.com/unifiedmoving_master?igsh=YjNtdGF0cWNncWF0&utm_source=qr"><i className="fab fa-instagram"></i></a></li>
+              <li><a href="https://wa.me/13653563255"><i className="fab fa-whatsapp"></i></a></li>
+              <li><a href="https://www.linkedin.com/in/abdulahi-lawal-897837357?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"><i className="fab fa-linkedin-in"></i></a></li>
+              <li><a href="https://www.tiktok.com/@unifiedmovingmaster?_t=ZM-8w0ScrIvJEU&_r=1"><i className="fab fa-tiktok"></i></a></li>
             </ul>
           </div>
         </nav>
